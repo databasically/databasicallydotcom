787ebc5d68e895d9ebf61efb359f5b49cba58155
Willis F. Jackson III <raidercat@gmail.com>
2013-03-13 10:05:56 -0500

    setting up one pager

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Databasically::Application.config.secret_token = '5c8847be47e9f6f9115cc505b34eb33d20fb38efafcdd8a0bd9195a14d9607efac120893aeee9fdeec7721413d5c3f6c0d2bd71b3e5712f73df5125ea4d48d65'
