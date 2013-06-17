69e497e19f4069482ec5a634020bcbf5fe280480
Wes Garrison <wes@wesgarrison.us>
2013-06-17 00:34:33 -0500

    Convert to middleman static site generator
    How to get started:
     * bundle install
     * (bundle exec) middleman 
    
    More details: 
      http://middlemanapp.com/getting-started/

---
+++ source/javascripts/all.js
@@ -0,0 +1,24 @@
+//= require jquery
+//= require google-code-prettify/prettify
+//= require bootstrap-transition
+//= require bootstrap-alert
+//= require bootstrap-modal
+//= require bootstrap-dropdown
+//= require bootstrap-scrollspy
+//= require bootstrap-tab
+//= require bootstrap-tooltip
+//= require bootstrap-popover
+//= require bootstrap-button
+//= require bootstrap-collapse
+//= require bootstrap-carousel
+//= require bootstrap-typeahead
+//= require bootstrap-affix
+//= require application
+//= require jquery.slabtext
+//= require jquery.isotope.min
+//= require custom
+//= require jquery.form
+
+
+
+// -- //= require_tree .
