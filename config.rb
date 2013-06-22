2a5c09cf1305d8a94cec9788c3b11027f0562f79
Wes Garrison <wes@wesgarrison.us>
2013-06-22 14:39:55 -0500

    New middleman config for new version+bloggin

--- config.rb
+++ config.rb
@@ -1,6 +1,42 @@
 ###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+# Reload browser when files change
+activate :livereload
+
+activate :blog do |blog|
+  # blog.prefix = "blog"
+  # blog.permalink = ":year/:month/:day/:title.html"
+  # blog.sources = ":year-:month-:day-:title.html"
+  # blog.taglink = "tags/:tag.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = ":year.html"
+  # blog.month_link = ":year/:month.html"
+  # blog.day_link = ":year/:month/:day.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/:num"
+end
+
+page "/feed.xml", :layout => false
+
+###
 # Compass
 ###
+
+# Susy grids in Compass
+# First: gem install susy
+# require 'susy'
 
 # Change Compass configuration
 # compass_config do |config|
@@ -24,9 +60,10 @@
 #   page "/admin/*"
 # end
 
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# Proxy (fake) files
+# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
+#   @which_fake_page = "Rendering a fake page with a variable"
+# end
 
 ###
 # Helpers
@@ -34,9 +71,6 @@
 
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -60,17 +94,18 @@ configure :build do
   # activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  # activate :cache_buster
 
   # Use relative URLs
   # activate :relative_assets
 
+  # Compress PNGs after build
+  # First: gem install middleman-smusher
+  # require "middleman-smusher"
+  # activate :smusher
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
-end
-
-activate :blog do |blog|
-  # set options on blog
 end
 
 activate :directory_indexes
